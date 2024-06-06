3914c6480c03e7df1ab741f7f74670d934d0d91d
aaustinoduor <aaustinoduorr@gmail.com>
2024-06-06 13:59:18 +0000

    python_variable_annotations

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,7 +3,6 @@ from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-
     zoomed_in: List = [
         item for item in lst
         for i in range(int(factor))
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,5 +4,6 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """returns tuple of a string and a float"""
     t = v ** 2
     return (k, t)
